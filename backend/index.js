0eb6c798ef8317220874895a421cb61f28c1aa46
prince6019 <prince.sharma6019@gmail.com>
2024-03-30 16:23:06 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,23 @@
-const { default: axios } = require("axios");
-const { networks } = require("bitcoinjs-lib");
-const { bitcoin } = require("bitcoinjs-lib/src/networks");
-const { ethers, providers } = require("ethers");
+const bip39 = require("bip39");
 const express = require("express");
+const ecc = require("tiny-secp256k1");
+const { BIP32Factory } = require("bip32");
+const bitcoin = require("bitcoinjs-lib");
+const { default: ECPairFactory } = require("ecpair");
+// You must wrap a tiny-secp256k1 compatible implementation
+const bip32 = BIP32Factory(ecc);
+require("dotenv").config();
 
 const app = express();
 
+const now_nodes_api_key = process.env.now_nodes_api_key;
+
 const port = 3000;
-// const ethMainnet = "https://eth.nownodes.io/"
-// const ethTestnet = `eth-sepolia.nownodes.io/${pro}`
-const createBitcoinWallet = async () => {
-  console.log(" creation of bitcoin wallet in progress -----");
-};
+const ethMainnet = `https://eth.nownodes.io/${now_nodes_api_key}`;
+const ethTestnet = `https://eth-sepolia.nownodes.io/${now_nodes_api_key}`;
 
-function deriveFiveWalletsFromHdNode(
-  mnemonic,
-  derivationPath,
-  numberOfAccounts
-) {
+function deriveWalletsFromHdNode(mnemonic, derivationPath, numberOfAccounts) {
   let wallets = [];
-
   for (let i = 0; i < numberOfAccounts; i++) {
     let hdNode = ethers.utils.HDNode.fromMnemonic(mnemonic).derivePath(
       derivationPath + "/" + i
@@ -28,27 +26,28 @@ function deriveFiveWalletsFromHdNode(
     let wallet = new ethers.Wallet(hdNode.privateKey);
     wallets.push(wallet);
   }
-  const provider = new ethers.providers.JsonRpcProvider("");
   return wallets;
 }
 
-async function signTransaction(wallet, toAddress, value) {
-  let transaction = {
-    nonce: 0,
+async function sendTransaction(walletMnemonic, toAddress, value) {
+  const ethNetwork = ethTestnet;
+  const provider = new ethers.providers.JsonRpcProvider(ethNetwork);
+  const wallet = walletMnemonic.connect(provider);
+  const nonce = await wallet.getTransactionCount();
+  let tx = {
+    nonce: nonce,
     gasLimit: 21000,
     gasPrice: ethers.utils.bigNumberify("2000000000"),
     to: toAddress,
     value: ethers.utils.parseEther(value),
     data: "0x",
   };
-  sendTransaction(wallet);
+
+  await wallet.sendTransaction(tx);
 }
 
-const sendTransaction = async (wallet) => {
-  const provider = new ethers.providers.JsonRpcProvider(
-    "https://eth.nownodes.io/14188213-eac0-40f4-a03d-7aec5f671ebb"
-  );
-  const signer = wallet.connect(provider);
+const restoreEthereumWallet = async (mnemonic) => {
+  return ethers.Wallet.fromMnemonic(mnemonic);
 };
 
 const createEthereumWallet = async () => {
@@ -68,6 +67,44 @@ const createEthereumWallet = async () => {
   console.log(xpriv, xpub, address);
 
   //   let derivationPath = "m/44'/60'/0'/0";
+};
+
+const derivedWalletsFromBitcoinNode = async (
+  root,
+  derivationPath,
+  numberOfAccounts
+) => {
+  let wallets = [];
+  const network = bitcoin.networks.bitcoin;
+  // bip49 derived path for mainnet =0 , testnet =1
+  for (let i = 0; i < numberOfAccounts; i++) {
+    let childNode = root.derivePath(derivationPath + "/" + i);
+
+    console.log(childNode);
+    console.log("privatekey :", childNode.privateKey.toString("hex"));
+    const address = bitcoin.payments.p2pkh({
+      pubkey: childNode.publicKey,
+      network,
+    }).address;
+    console.log(address);
+    console.log("public key : ", childNode.publicKey.toString("hex"));
+    wallets.push(childNode.toBase58());
+  }
+  const path = "m/49'/1'/0'/0";
+  const child = root.derivePath(path);
+};
+
+const createBitcoinWallet = async () => {
+  // generate mnemonic
+  const mnemonic = bip39.generateMnemonic();
+  console.log(mnemonic);
+  const seed = bip39.mnemonicToSeedSync(mnemonic);
+  const root = bip32.fromSeed(seed);
+  console.log("root : ", root);
+  console.log("priv : ", root.privateKey.toString("hex"));
+  console.log("child nodes ----------------");
+  const path = "m/49'/1'/0'/0";
+  derivedWalletsFromBitcoinNode(root, path, 2);
 };
 
 // create ethereum for the user -------------------
---
+++ backend/readme.md
@@ -0,0 +1,5 @@
+git add .
+git commit
+git pull origin main
+git push on prince branch
+merge through github
